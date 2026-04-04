7244b2511ad9dd6cd4c1662c6325a0ebb55eb84c
Michelle Magdalene <michellemagdalene885@gmail.com>
2026-04-04 22:07:15 +0530

    Updated slider images in index.html

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = 'msmtoursandtravels2026@gmail.com'
-app.config['MAIL_PASSWORD'] = 'thixyxqdkhcqfmoq'
+app.config['MAIL_PASSWORD'] = 'wotldzkyjzkysvvw'
 
 mail = Mail(app)
 
